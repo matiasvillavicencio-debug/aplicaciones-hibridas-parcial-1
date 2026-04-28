f3c284abe6c4b7e54e2d27bd7c52473deede7bb0
Matias <matias.villavicencio@davinci.edu.ar>
2026-04-28 10:33:50 -0300

    Añadido

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,13 +1,24 @@
-const validarToken = (req, res, next) => {
-    console.log('Validando token');
+import dotenv from 'dotenv'
+import jsonwebtoken from 'jsonwebtoken'
+
+dotenv.config();
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const validarToken = ( req, res, next) =>{
     const token = req.headers.authorization;
-
-    if(!token){
-        res.status(401).json({msg: 'No se paso el JWT'});
+    if( !token){
+        res.status(401).json({ msg: 'No se paso el JWT'});
     }
-    console.log({token});
-
+    // Verificamos el token
+    const jwt = token.split(' ')[1];
+    console.log(jwt);
+    jsonwebtoken.verify( jwt, SECRET_KEY, (error, decoded) => {
+        if( error){
+            res.status(403).json({msg: 'Token Invalido'});
+        }
+        console.log( { decoded})
+        req.body.userId = decoded._id;
+    })
     next();
 }
-
 export default validarToken;
