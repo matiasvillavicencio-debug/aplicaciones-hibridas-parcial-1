e9a0c308cb29c718b88cd8d0db686408e481b1cd
Matias <matias.villavicencio@davinci.edu.ar>
2026-04-28 10:48:01 -0300

    Añadido

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const clase = 'Semana 07';
+  const span = <span> Algo </span>;
+
+  return (
+    <h1> React { clase } { span } </h1>
+  )
+}
+
+export default App
